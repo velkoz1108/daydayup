7ee06c338c254d423c88b23e1f0ebf32cbf8e0dd
haotingfang <haotingfang@souche.com>
2020-09-03 15:04:41 +0800

    add test-demo

---
+++ test-demo/src/test/java/com/example/demo/TestDemoApplicationTests.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@SpringBootTest
+class TestDemoApplicationTests {
+
+
+    /*java 8 test*/
+    @Test
+    void test() {
+        List<TestBean> list=new ArrayList<>();
+        for(int i=0;i<=10;i++){
+            TestBean testBean=TestBean.builder().id("id_"+i).name("name_"+i).build();
+            list.add(testBean);
+        }
+        // 获取对应的平方数
+        List<String> squaresList = list.stream().map(TestBean::getId).distinct().collect(Collectors.toList());
+        System.out.println(squaresList);
+
+        List<String> vlist=new ArrayList<>();
+        vlist.add("id_10");
+
+        List<TestBean> squaresList1=list.stream().filter(ss->vlist.contains(ss.getId())).collect(Collectors.toList());
+        System.out.println(squaresList1);
+
+
+        List<String>strings = Arrays.asList("abc", "", "bc", "efg", "abcd","", "jkl");
+// 获取空字符串的数量
+        long count = strings.stream().filter(string -> string.isEmpty()).count();
+        System.out.println("count:"+count);
+    }
+
+
+}
